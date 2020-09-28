47b6fe1cd696be41bc6e37b5e7eb4d50524bb1d8
Cristian Incarnato <cristian.cdi@gmail.com>
2020-09-28 07:27:05 -0300

    Add childRoles to user delete

--- packages/api/src/graphql/resolvers/UserResolvers.js
+++ packages/api/src/graphql/resolvers/UserResolvers.js
@@ -22,76 +22,95 @@ import {
 
 export default {
     Query: {
+
         users: (_, {}, {user, rbac}) => {
             if (!user) throw new AuthenticationError("UNAUTHENTICATED")
             if (!user || !rbac.isAllowed(user.id, SECURITY_USER_SHOW)) throw new ForbiddenError("Not Authorized")
             return findUsers(user.role.childRoles)
         },
+
         user: (_, {id}, {user, rbac}) => {
             if (!user) throw new AuthenticationError("UNAUTHENTICATED")
             if (!user || !rbac.isAllowed(user.id, SECURITY_USER_SHOW)) throw new ForbiddenError("Not Authorized")
             return findUser(id)
         },
+
         paginateUsers: (_, {limit, pageNumber, search, orderBy, orderDesc}, {user, rbac}) => {
             if (!user) throw new AuthenticationError("UNAUTHENTICATED")
             if (!user || !rbac.isAllowed(user.id, SECURITY_USER_SHOW)) throw new ForbiddenError("Not Authorized")
 
             return paginateUsers(limit, pageNumber, search, orderBy, orderDesc, user.role.childRoles)
         },
+
     },
     Mutation: {
 
         createUser: (_, {input}, {user, rbac}) => {
             if (!user) throw new AuthenticationError("UNAUTHENTICATED")
+            if (!user || !rbac.isAllowed(user.id, SECURITY_USER_CREATE)) throw new ForbiddenError("Not Authorized")
 
 
-            if (rbac.isAllowed(user.id, SECURITY_USER_CREATE)){
-
-                //With childRoles
-                if(user.role.childRoles.length > 0){
-                    //Check if role is include as a childRole
-                    if(user.role.childRoles.includes(input.role)){
-                        return createUser(input, user)
-                    }else{
-                        throw new ForbiddenError("Not Authorized")
-                    }
-                //Whitout childRoles
-                }else{
+            //With childRoles
+            if (user.role.childRoles.length) {
+                //Check if role is include as a childRole
+                if (user.role.childRoles.includes(input.role)) {
                     return createUser(input, user)
+                } else {
+                    throw new ForbiddenError("Not Authorized")
                 }
 
-            } else{
-                throw new ForbiddenError("Not Authorized")
+            //Without childRoles
+            } else {
+                return createUser(input, user)
             }
+
         },
+
         updateUser: (_, {id, input}, {user, rbac}) => {
             if (!user) throw new AuthenticationError("UNAUTHENTICATED")
+            if (!user || !rbac.isAllowed(user.id, SECURITY_USER_EDIT)) throw new ForbiddenError("Not Authorized")
 
-            if (rbac.isAllowed(user.id, SECURITY_USER_EDIT)){
-
-                //With childRoles
-                if(user.role.childRoles.length > 0){
-                    //Check if role is include as a childRole
-                    if(user.role.childRoles.includes(input.role)){
-                        return updateUser(id, input, user)
-                    }else{
-                        throw new ForbiddenError("Not Authorized")
-                    }
-                    //Whitout childRoles
-                }else{
+            //With childRoles
+            if (user.role.childRoles.length) {
+                //Check if role is include as a childRole
+                if (user.role.childRoles.includes(input.role)) {
                     return updateUser(id, input, user)
+                } else {
+                    throw new ForbiddenError("Not Authorized")
                 }
 
-            } else{
-                throw new ForbiddenError("Not Authorized")
+            //Without childRoles
+            } else {
+                return updateUser(id, input, user)
+            }
+
+
+        },
+
+        deleteUser:  async (_, {id}, {user, rbac}) => {
+            if (!user) throw new AuthenticationError("UNAUTHENTICATED")
+            if (!user || !rbac.isAllowed(user.id, SECURITY_USER_DELETE)) throw new ForbiddenError("Not Authorized")
+
+
+            //With childRoles
+            if (user.role.childRoles.length) {
+                //Check if role is include as a childRole
+
+                let userToDelete = await findUser(id)
+
+                if (user.role.childRoles.includes(userToDelete.role.id)) {
+                    return deleteUser(id, user)
+                } else {
+                    throw new ForbiddenError("Not Authorized")
+                }
+
+            //Without childRoles
+            } else {
+                return deleteUser(id, user)
             }
 
         },
-        deleteUser: (_, {id}, {user, rbac}) => {
-            if (!user) throw new AuthenticationError("UNAUTHENTICATED")
-            if (!user || !rbac.isAllowed(user.id, SECURITY_USER_DELETE)) throw new ForbiddenError("Not Authorized")
-            return deleteUser(id, user)
-        },
+
         changePasswordAdmin: (_, {id, password, passwordVerify}, {user, rbac}) => {
             if (!user) throw new AuthenticationError("UNAUTHENTICATED")
             if (!user || !rbac.isAllowed(user.id, SECURITY_USER_EDIT)) throw new ForbiddenError("Not Authorized")
